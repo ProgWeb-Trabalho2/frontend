19301e16673503450331552141ddd00c3ea6346b
João Marcello Amaral <marcello.jao1611@gmail.com>
2025-11-29 03:24:31 -0300

    Consertando review edit

--- public/javascript/review-edit.js
+++ public/javascript/review-edit.js
@@ -12,31 +12,11 @@ function loadEditForm() {
         const index = Number(localStorage.getItem("editReviewId"));
         const list = JSON.parse(localStorage.getItem("reviews") || "[]");
         const review = list[index];
-        const res = yield fetch("./data/games.json");
-        const games = yield res.json();
-        const select = document.getElementById("game-select");
-        games.forEach((g) => {
-            const opt = document.createElement("option");
-            opt.value = g.id.toString();
-            opt.textContent = g.name;
-            if (opt.value === review.game)
-                opt.selected = true;
-            select.appendChild(opt);
-        });
-        document.getElementById("score").value = review.score;
-        document.getElementById("comment").value =
-            review.comment;
-        document.getElementById("update-btn").addEventListener("click", () => {
-            review.game = select.value;
-            review.score = document.getElementById("score").value;
-            review.comment = document.getElementById("comment").value;
-            list[index] = review;
-            localStorage.setItem("reviews", JSON.stringify(list));
-            localStorage.removeItem("editReviewId");
-            window.location.href = "profile.html";
-        });
+        console.log(list);
     });
 }
-loadEditForm();
+onload = () => {
+    loadEditForm();
+};
 export {};
 //# sourceMappingURL=review-edit.js.map
